28194c2d5986bbf32005d2824ae0d2af22dc2e4f
Raymond Amenah <raymondamenah@gmail.com>
2023-06-06 04:15:59 +0100

    initial commit

---
+++ client/src/scenes/dashboard/Row2.tsx
@@ -0,0 +1,12 @@
+import DashboardBox from "@/components/dasboardBox"
+
+const Row2 = () => {
+    return (
+        <>
+            <DashboardBox gridArea='d'></DashboardBox>
+            <DashboardBox gridArea='e'></DashboardBox>
+            <DashboardBox gridArea='g'></DashboardBox>
+        </>
+    )
+}
+export default Row2
---
+++ client/src/scenes/dashboard/Row3.tsx
@@ -0,0 +1,12 @@
+import DashboardBox from "@/components/dasboardBox"
+
+const Row3 = () => {
+    return (
+        <>
+            <DashboardBox gridArea='h'></DashboardBox>
+            <DashboardBox gridArea='i'></DashboardBox>
+            <DashboardBox gridArea='j'></DashboardBox>
+        </>
+    )
+}
+export default Row3
